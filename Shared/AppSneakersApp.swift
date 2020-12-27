e3742b0f81b2f1d2d0352240c09a7933a8ff673a
Oswaldo Rubio <oswaldo.rubio@xing.com>
2020-12-27 15:59:02 +0100

    Initial Commit

---
+++ Shared/AppSneakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSneakersApp.swift
+//  Shared
+//
+//  Created by Oswaldo Rubio on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct AppSneakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Oswaldo Rubio on 27/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
